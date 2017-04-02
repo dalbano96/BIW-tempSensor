2461f277a2b39231f529bc9616439fadfcadc0cf
root <dalbano96@gmail.com>
2017-04-02 01:22:13 -1000

    Regex works for specified devices. IOW, it is not compatible with random files but rather, the given attributes for the BIW sensors

--- demoCore.py
+++ demoCore.py
@@ -16,7 +16,7 @@ class DemoCore():
 
     def index(self,request):
         ser = serial.Serial('/dev/ttyACM0', 9600) # This is the Serial Port we gather information from. You could replace this with the inbuild port if you wish
-	data = ["","","",""]
+	data = ["","","","",""]
 
 #	form = cgi.FieldStorage() # Prep demoCore to receive input data
 #        toggle = form.getvalue("toggle") # Retrieve user toggle, important for toggling device
@@ -35,7 +35,7 @@ class DemoCore():
 
     def receiving(self, ser):
 	global last_received
-	data = ["","","",""]
+	data = ["","","","",""]
 	buffer = ''
 	while True:
 		buffer = buffer + ser.read()
@@ -50,13 +50,18 @@ class DemoCore():
 				if '\r\n' in buffer:
 					# buffer in this scope stores the entire println string
 					data[0] = buffer
-					data[3] = data[0] 
 					matchTemperature = re.match(r'(.*)Temperature: ([0-9]*)', buffer, re.M|re.I)
 					if matchTemperature:
 						data[1] = matchTemperature.group(2)
 					matchPH = re.match(r'(.*)pH Levels: ([0-9]*)', buffer, re.M|re.I)
 					if matchPH:
 						data[2] = matchPH.group(2)
+					matchOxygen = re.match(r'(.*)Dissolved Oxygen: ([0-9]*)', buffer, re.M|re.I)
+					if matchOxygen:
+						data[3] = matchOxygen.group(2)
+					matchEConductivity = re.match(r'(.*)Electrical Conductivity: ([0-9]*)', buffer, re.M|re.I)
+					if matchEConductivity:
+						data[4] = matchEConductivity.group(2)
 					return data
 			
 
@@ -84,11 +89,11 @@ class DemoCore():
 	html = html.replace("%Temperature%",str(data[1]))
 
 	# used to test/debug data
-	html = html.replace("%tempData%",str(data[3]))
+	# html = html.replace("%tempData%",str(data[3]))
 
 	html = html.replace("%pH%",str(data[2]))
-	# html = html.replace("%DissolvedOxygen%",str(data[2]))
-	# html = html.replace("%ElectricalConductivity%",str(data[3]))
+	html = html.replace("%DissolvedOxygen%",str(data[3]))
+	html = html.replace("%ElectricalConductivity%",str(data[4]))
 
         return html
 
